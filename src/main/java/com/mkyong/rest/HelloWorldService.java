31beb676a240d2d45af34b4e753e0152f5ed8dc8
Mathias Hultman <mathiashultman@Mathiass-MacBook-Pro.local>
2017-08-29 20:16:45 +0200

    updated to consume a person

--- src/main/java/com/mkyong/rest/HelloWorldService.java
+++ src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -1,16 +1,21 @@
 package com.mkyong.rest;
 
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-// http://localhost:8080/170829-jersey-with-webxml/rest/h/m
+// http://localhost:8080/170829-jersey-with-webxml/rest/h/post
 
 @Path("/h")
 public class HelloWorldService {
+	private Logger logger = Logger.getLogger(HelloWorldService.class.getName());
 
 	@GET
 	@Path("/h/{param}")
@@ -27,5 +32,12 @@ public class HelloWorldService {
 		Person p = new Person();
 		p.setName("huuu");
 		return p;
+	}
+	
+	@POST
+	@Path("/post")
+	@Consumes({MediaType.APPLICATION_JSON})
+	public void setPerson(Person p) {
+		logger.warning(p.toString());
 	}
 }
